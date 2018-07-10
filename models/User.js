9e3b7d3d36f0a52406855aeccc06c01f35ebb620
Ivan Arredondo <ivan.arredondo@mail.mcgill.ca>
2018-07-10 16:04:37 -0400

    set up mongodB and passport

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+	//you can freely add or subtract properties, no issue
+	googleId: String
+});
+
+mongoose.model('users', userSchema); //tells mongoose we want to make a new collection called users, this also loads it into mongoose so we can use it throughout the app
---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users'); //fetching the model we added in User.js
+
+passport.serializeUser((user, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientId,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				//mongoose queries return promises
+				if (existingUser) {
+					done(null, existingUser);
+				} else {
+					new User({ googleId: profile.id }) //creates a mongoose model instance
+						.save() //.save() actually adds this to the database
+						.then(user => done(null, user)); //we make sure to call done if the promise returns so that we dont move on before we have actually saved it in the db
+				}
+			});
+		}
+	)
+);
