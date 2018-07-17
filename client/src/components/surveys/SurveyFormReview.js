6d11a22b134a2ab8b0948d72cfc19cd8fe13e3c3
Ivan Arredondo <ivan.arredondo@mail.mcgill.ca>
2018-07-17 12:08:22 -0400

    sending a survey flow complete

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,25 +1,51 @@
 //shows users what they are about to send in the survey form that they just filled out
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
-  return (
-    <div>
-      <h5>Please confirm your entries</h5>
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
+	const reviewFields = _.map(formFields, ({ name, label }) => {
+		return (
+			<div key={name}>
+				<label>{label}</label>
+				<div>{formValues[name]}</div>
+			</div>
+		);
+	});
 
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
-        Back
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<h5>Please confirm your entries</h5>
+			{reviewFields}
+			<button
+				className="yellow darken-3 white-text btn-flat"
+				onClick={onCancel}
+			>
+				Back
+			</button>
+			<button
+				onClick={() => submitSurvey(formValues, history)}
+				className="green white-text btn-flat right"
+			>
+				Send Survey
+				<i className="material-icons right">email</i>
+			</button>
+		</div>
+	);
 };
 
 //this function is where we declare what state vars we want
 function mapStateToProps(state) {
-  //console.log(state); //if you want to see what global state vars are available to you
-  return { formValues: state.form.surveyForm.values };
+	//console.log(state); //if you want to see what global state vars are available to you
+	return { formValues: state.form.surveyForm.values };
 }
 
 //we use connect when we want state variables from the global redux store
 //whatever we get from mapstatetoprops will be availble to us as a prop above
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(
+	mapStateToProps,
+	actions
+)(withRouter(SurveyFormReview));
--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,33 +1,38 @@
 //Survey new shows surveyForm and surveyFormReview
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { showFormReview: false };
-  // }
+	// constructor(props) {
+	//   super(props);
+	//   this.state = { showFormReview: false };
+	// }
 
-  state = { showFormReview: false }; //this is the same as above, its a new way of making the constructor, but fast
+	state = { showFormReview: false }; //this is the same as above, its a new way of making the constructor, but fast
 
-  renderContent() {
-    if (this.state.showFormReview) {
-      return (
-        <SurveyFormReview
-          onCancel={() => this.setState({ showFormReview: false })}
-        />
-      );
-    }
-    return (
-      <SurveyForm
-        onSurveySubmit={() => this.setState({ showFormReview: true })}
-      />
-    );
-  }
-  render() {
-    return <div>{this.renderContent()}</div>;
-  }
+	renderContent() {
+		if (this.state.showFormReview) {
+			return (
+				<SurveyFormReview
+					onCancel={() => this.setState({ showFormReview: false })}
+				/>
+			);
+		}
+		return (
+			<SurveyForm
+				onSurveySubmit={() => this.setState({ showFormReview: true })}
+			/>
+		);
+	}
+	render() {
+		return <div>{this.renderContent()}</div>;
+	}
 }
 
-export default SurveyNew;
+//we add this here because if we navigate away from this component, like we log out
+//we want the form values to get dumped, this is default but we unset the default in SurveyForm
+export default reduxForm({
+	form: 'surveyForm'
+})(SurveyNew);
